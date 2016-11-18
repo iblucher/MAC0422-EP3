f1c8b922d2b165da061c4e05eb9dd857bbcf8377
Isabela Blucher <isabelablucher@hotmail.com>
2016-11-18 14:09:09 -0200

    Coisas dando certo!

--- Simulator.java
+++ Simulator.java
@@ -9,18 +9,24 @@
 *******************************************************************************/
 
 import java.io.*;
-import java.util.*;
 
 public class Simulator {
 
-    static Simulator sim;
-    int total, virtual, s, p;
+    static Simulator sim; // objeto da classe Simulator
+    static BinaryOut outTot, outVir; // arquivos binários
+    long[] memTot, memVir; // vetores que representam o conteúdos dos arquivos
+    int total, virtual, s, p; // valores dados na primeira linha do trace
 
     public void createDirectory () {
-        File dir = new File ("tmp/");
+        File dir = new File ("/tmp/");
         if (!dir.exists()) {
             dir.mkdir();
         }
+
+        // criar arquivos binários
+        outTot = new BinaryOut("/tmp/ep3.mem");
+        outVir = new BinaryOut("/tmp/ep3.vir");
+
     }
 
     public void loadFile (String name) {
@@ -30,14 +36,32 @@ public class Simulator {
         s = in.readInt();
         p = in.readInt();
 
-        StdOut.println ("Arquivo é: " + name);
         StdOut.printf ("%d %d %d %d\n", total, virtual, s, p);
 
-        sim.createDirectory();
+        sim.initMemory();
 
-        while (!in.isEmpty()) {
+        //while (!in.isEmpty()) {
             // leitura do resto do trace (existe um vector para Java)
+            // criar uma estrutura que armazena as características dos processos (o que está no trace)
+        //}
+    }
+
+    public void initMemory () {
+        memTot = new long[total/s];
+        memVir = new long[virtual/p];
+
+        for (int i = 0; i < total/s; i++) {
+            memTot[i] = -1;
+            outTot.write(-1);
         }
+
+        for (int i = 0; i < virtual/p; i++) {
+            memVir[i] = -1;
+            outVir.write(-1);
+        }
+
+        outTot.flush();
+        outVir.flush();
     }
 
     public static void main (String[] args) throws java.io.IOException {
@@ -49,14 +73,17 @@ public class Simulator {
         String[] command = null;
         int espaco;
         int substitui;
+
+        sim.createDirectory();
+
         while (true) {
             StdOut.print ("(ep3): ");
             line = in.readLine();
 
-            command = line.split("-");
-
+            command = line.replaceAll("^\\s+", "").split(" ", -1);
+            
             // se der enter, nada acontece
-            if (command[0].equals(" "))
+            if (command[0].equals(""))
                 continue;
             // se o comando for "sai", finaliza o simulador
             else if (command[0].equals("sai"))
@@ -69,7 +96,6 @@ public class Simulator {
                 substitui = Integer.parseInt(command[1]);
             //else if (command[0].equals("executa"))
                 // execução 
-
         }
     }
 }
